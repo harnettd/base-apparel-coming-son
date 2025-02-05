b7b1c013d350a276cacea52e9559377f8ba054ec
Derek Harnett <DHarnett.dev@proton.me>
2025-02-05 15:49:34 -0800

    add documentation to index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+// Return block__element class name.
 const getBEClass = (block, element) => {
   return `${block}__${element}`;
 };
 
+// Return block__element--modifier class name.
 const getBEMClass = (block, element, modifier) => {
   return `${getBEClass(block, element)}--${modifier}`;
 };
 
+// blocks, elements, and modifiers.
 const formBlock = "email-form";
 const inputElement = "input";
 const errIconElement = "err-icon";
@@ -13,11 +16,13 @@ const errMessageElement = "err-msg";
 const buttonElement = "btn";
 const onErrModifier = "on-err";
 
+// HTML elements
 const input = document.querySelector("." + getBEClass(formBlock, inputElement));
 const errIcon = document.querySelector("." + getBEClass(formBlock, errIconElement));
 const errMessage = document.querySelector("." + getBEClass(formBlock, errMessageElement));
 const submitButton = document.querySelector("." + getBEClass(formBlock, buttonElement));
 
+// On window load, clear email input and error indicators.
 window.addEventListener("load", (event) => {
   input.value = null;
   errIcon.classList.remove(getBEMClass(formBlock, errIconElement, onErrModifier));
@@ -28,12 +33,11 @@ window.addEventListener("load", (event) => {
 const isValidEmail = (email) => {
   const emailRegexp = /^\w+(\.\w+)*@\w+(\.\w+)*$/g;
   return email.match(emailRegexp) ? true : false;
-
 };
 
+// On form submit, perform client-side validation of the email address.
 submitButton.addEventListener("click", (event) => {
   const email = input.value;
-  console.log(`email: ${email}`)
   if (isValidEmail(email)) {
     alert("Form data validated");
   } else {
